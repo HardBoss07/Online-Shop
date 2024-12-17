2feadaac89fad01bca7ebc6deede8982cce70f1b
HardBoss07 <matteo@bosshard.net>
2024-12-17 20:56:52 +0100

    Centralized styling by adding fonts and colors to tailwind.config.ts Reworked entire globals.css by changing regular css code to an optimized tailwind css code

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,9 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#3498db", // color1: primary brand color
+        secondary: "#2ecc71", // color2: secondary brand color
+        highlight: "#f39c12", // color3: primary highlight color
+        highlight2: "#e74c3c", // color4: secondary highlight color
+        navbar: "#34495e", // color5: nav bar color
+        offwhite: "#f5f5f5", // color6: off-white color
+        offblack: "#2c3e50", // color7: off-black color
       },
+      fontFamily: {
+        roboto: ["'Roboto'", "sans-serif"],
+      }
     },
   },
   plugins: [],
