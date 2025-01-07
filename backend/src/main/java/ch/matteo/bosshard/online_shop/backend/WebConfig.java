4b05126611bfbc9a23d5ea27c1ce6735d62a0310
HardBoss07 <matteo@bosshard.net>
2025-01-07 09:53:12 +0100

    Removed unnecessary imports

--- backend/src/main/java/ch/matteo/bosshard/online_shop/backend/WebConfig.java
+++ backend/src/main/java/ch/matteo/bosshard/online_shop/backend/WebConfig.java
@@ -1,11 +1,8 @@
 package ch.matteo.bosshard.online_shop.backend;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
